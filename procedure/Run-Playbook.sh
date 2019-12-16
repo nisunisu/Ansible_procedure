007203872ef7e627ade0eda056e633cd5a9d9c3c
nisunisu <sitos.nishi@gmail.com>
2019-12-17 08:45:22 +0900

    Update : ローカル実行の手順を追加

--- procedure/Run-Playbook.sh
+++ procedure/Run-Playbook.sh
@@ -2,4 +2,7 @@
 ansible-playbook ${HOME}/ansible/site.yml
 
 # インベントリファイルを指定
-ansible-playbook -i ${HOME}/ansible/hosts ${HOME}/ansible/site.yml
+ansible-playbook ${HOME}/ansible/site.yml --inventory-file=${HOME}/ansible/hosts
+
+# ローカルに対して実行
+ansible-playbook ${HOME}/ansible/site.yml --connection=local
